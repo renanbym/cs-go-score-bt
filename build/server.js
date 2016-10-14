04bb42320b02d2631d31bc8456d06b88d9c08e25
Renan Mariano <renan.mariano@f2f-digital.com>
2016-10-14 18:48:20 -0300

    debug heroku

---
+++ build/server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+var hltv = require('./app.js');
+
+server.connection({
+    host: 'localhost',
+    port: 3000
+});
+
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function handler(request, reply) {
+        return reply('hello world');
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/hltv',
+    handler: function handler(request, reply) {
+        hltv.requestMatch(function (err, matchs) {
+            if (err) return reply(err).code(406);
+            return reply(matchs);
+        });
+    }
+});
+
+server.start(function (err) {
+    if (err) throw err;
+    console.log('Server running at:', server.info.uri);
+});
---
+++ source/server.js
@@ -0,0 +1,32 @@
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+const hltv = require('./app.js');
+
+server.connection({
+    host: 'localhost',
+    port: 3000
+});
+
+server.route({
+    method: 'GET',
+    path:'/hello',
+    handler: function (request, reply) {
+        return reply('hello world');
+    }
+});
+server.route({
+    method: 'GET',
+    path:'/hltv',
+    handler: function (request, reply) {
+        hltv.requestMatch( (err, matchs) => {
+            if (err) return reply(err).code(406);
+            return reply(matchs);
+        })
+    }
+});
+
+server.start((err) => {
+    if (err) throw err;
+    console.log('Server running at:', server.info.uri);
+});
