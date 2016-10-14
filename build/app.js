23e8367528abd3a3ac1d7461f62a8977d16646d3
Renan Mariano <renan.mariano@f2f-digital.com>
2016-10-14 17:56:39 -0300

    achoh que foi

--- build/app.js
+++ build/app.js
@@ -1,65 +1,63 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
+var moment = require('moment');
+
+var getMatchs = function getMatchs(data, callback) {
+    var $ = cheerio.load(data);
+    var match = [];
+
+    var inGames = $('#secondCollumn a');
+    for (var i = 0; i < inGames.length; i++) {
+        var title = $('#secondCollumn a').eq(i).attr('title');
+        var href = $('#secondCollumn a').eq(i).attr('href');
+        var matchBox = $('#secondCollumn a .frontpageMatchBox').eq(i).html();
+        var img = $('#secondCollumn a .frontpageMatchBox').eq(i).find('img.flagFix');
+
+        var t1 = $('#secondCollumn a .frontpageMatchBox').eq(i).find('img.flagFix').eq(0).parent();
+        var t2 = $('#secondCollumn a .frontpageMatchBox').eq(i).find('img.flagFix').eq(1).parent();
+
+        if (typeof title != 'undefined' && typeof href != 'undefined' && matchBox != null) {
+
+            var r1 = '*';
+            var r2 = '*';
+
+            var time1 = t1.find('span').html();
+            var time2 = t2.find('span').html();
+
+            var horario = /[0-9]{2}\:[0-9]{2}/.exec(matchBox);
+
+            if (horario == null) {
+                time1 = /\<span.*\>.*\<\/span.*\>.*\<span.*\>(.*)\<\/span.*\>/.exec(t1.parent().html());
+                time2 = /\<span.*\>.*\<\/span.*\>.*\<span.*\>(.*)\<\/span.*\>/.exec(t2.parent().html());
+                horario = 'live';
+            } else {
+
+                // horario = moment('2016-12-12 '+horario[0]+':00').subtract( 2, 'hour').format('HH:mm');
+                horario = horario[0];
+            }
+
+            match.push({
+                title: title,
+                link: href,
+                t1: (typeof time1 === 'undefined' ? 'undefined' : _typeof(time1)) == 'object' ? time1[1] : time1,
+                t2: (typeof time2 === 'undefined' ? 'undefined' : _typeof(time2)) == 'object' ? time2[1] : time2,
+                time: horario,
+                img1: img.eq(0).attr('src'),
+                img2: img.eq(1).attr('src')
+            });
+        }
+    }
+
+    callback(null, match);
+};
 
 request('http://www.hltv.org', function (err, response, body) {
-    var $ = cheerio.load(body);
-
-    var inGames = $('#secondCollumn .starred, #secondCollumn .notStarred');
-
-    // inGames.each( ( index, games ) => {
-    var games = inGames[0];
-    // console.log(games);
-    console.log(games.attribs.title);
-
-    var frontpageMatchBox = games.children[1];
-
-    var t = frontpageMatchBox.children[1].children[2];
-
-    l.data;
-    console.log(t);
-
-    return true;
-    // })
+    getMatchs(body, function (err, match) {
+        console.log(match);
+    });
 });
-
-// <a class="notStarred" title="WESG 2016 Europe & CIS Regional Finals" href="/match/2305416-russia-bpro-wesg-2016-europe-cis-regional-finals">
-//0     <div class="frontpageMatchBox" style="background-image: url(http://static.hltv.org//images/digital/emptybutton.gif);opacity: 0.4">
-//
-//0       <div style="display: flex;flex-direction: column;justify-content: space-between;padding-left:2px;padding-top:3px;height:26px;padding-right: 2px;;">
-//
-//0             <div style="display:flex;flex-direction: row;justify-content: space-between;">
-//
-// 0                <div style="text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">
-//  0                    <span style="display:inline-block;width:25px;text-align: center;">
-//   1                        <img src="http://static.hltv.org//images/flag/ru.gif" alt="" height="12" width="18" class="flagFix"/>
-//  0                    </span>
-//  1                    <span style="">.Russia</span>
-// 0                </div>
-//
-// 1                <div style="flex-shrink: 0;padding-left:2px;padding-right:2px;">
-//  0                    <span id="hm_livescore_2305416_7268">0</span>
-// 1                </div>
-//0            </div>
-//
-//1            <div style="display:flex;flex-direction: row;justify-content: space-between">
-//0                <div style="text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">
-//
-//0                    <span style="display:inline-block;width:25px;text-align: center;">
-// 0                        <img src="http://static.hltv.org//images/flag/bg.gif" alt="" height="12" width="18" class="flagFix"/>
-//0                    </span>
-//
-//1                    <span style="">Bpro</span>
-//0                </div>
-//1                <div style="flex-shrink: 0;padding-left:2px;padding-right:2px;">
-//0                    <span id="hm_livescore_2305416_6762">0 </span>
-//1                </div>
-//1            </div>
-//
-//0         </div>
-//
-//0     </div>
-//
-// </a>
-//# sourceMappingURL=app.js.map
--- source/app.js
+++ source/app.js
@@ -1,68 +1,62 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const moment = require('moment');
+
+const getMatchs = ( data, callback ) =>{
+    let $ = cheerio.load(data);
+    let match = [];
+
+    let inGames = $('#secondCollumn a');
+    for( var i = 0 ; i < inGames.length; i++ ){
+        let title = $('#secondCollumn a').eq(i).attr('title');
+        let href = $('#secondCollumn a').eq(i).attr('href');
+        let matchBox = $('#secondCollumn a .frontpageMatchBox').eq(i).html();
+        let img = $('#secondCollumn a .frontpageMatchBox').eq(i).find('img.flagFix');
+
+        let t1 = $('#secondCollumn a .frontpageMatchBox').eq(i).find('img.flagFix').eq(0).parent();
+        let t2 = $('#secondCollumn a .frontpageMatchBox').eq(i).find('img.flagFix').eq(1).parent();
+
+        if( typeof title != 'undefined' &&  typeof href != 'undefined' && matchBox != null  ){
+
+            let r1 = '*';
+            let r2 = '*';
+
+            let time1 = t1.find('span').html();
+            let time2 = t2.find('span').html();
+
+            let horario = /[0-9]{2}\:[0-9]{2}/.exec( matchBox );
 
 
+            if( horario == null ){
+                time1 = /\<span.*\>.*\<\/span.*\>.*\<span.*\>(.*)\<\/span.*\>/.exec( t1.parent().html() );
+                time2 = /\<span.*\>.*\<\/span.*\>.*\<span.*\>(.*)\<\/span.*\>/.exec( t2.parent().html() );
+                horario = 'live';
+            }else{
+
+                // horario = moment('2016-12-12 '+horario[0]+':00').subtract( 2, 'hour').format('HH:mm');
+                horario = horario[0]
+            }
+
+            match.push({
+                title: title
+                , link: href
+                ,t1:  ( typeof time1 == 'object' ? time1[1] : time1 )
+                ,t2:  ( typeof time2 == 'object' ? time2[1] : time2 )
+                ,time: horario
+                ,img1: img.eq(0).attr('src')
+                ,img2: img.eq(1).attr('src')
+            })
+        }
+
+    }
+
+    callback(null, match);
+
+}
 
 request('http://www.hltv.org', (err, response, body) => {
-        let $ = cheerio.load(body);
-
-        let inGames = $('#secondCollumn .starred, #secondCollumn .notStarred');
-
-
-        // inGames.each( ( index, games ) => {
-        let games = inGames[0];
-        // console.log(games);
-            console.log(games.attribs.title);
-
-            let frontpageMatchBox = games.children[1];
-
-            let t = frontpageMatchBox.children[1].children[2];
-
-            l.data;
-            console.log(t);
-
-
-
-            return true;
-        // })
+    getMatchs( body, (err, match ) => {
+        console.log(match);
+    } );
 })
-
-
-// <a class="notStarred" title="WESG 2016 Europe & CIS Regional Finals" href="/match/2305416-russia-bpro-wesg-2016-europe-cis-regional-finals">
-//0     <div class="frontpageMatchBox" style="background-image: url(http://static.hltv.org//images/digital/emptybutton.gif);opacity: 0.4">
-//
-//0       <div style="display: flex;flex-direction: column;justify-content: space-between;padding-left:2px;padding-top:3px;height:26px;padding-right: 2px;;">
-//
-//0             <div style="display:flex;flex-direction: row;justify-content: space-between;">
-//
-// 0                <div style="text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">
-//  0                    <span style="display:inline-block;width:25px;text-align: center;">
-//   1                        <img src="http://static.hltv.org//images/flag/ru.gif" alt="" height="12" width="18" class="flagFix"/>
-//  0                    </span>
-//  1                    <span style="">.Russia</span>
-// 0                </div>
-//
-// 1                <div style="flex-shrink: 0;padding-left:2px;padding-right:2px;">
-//  0                    <span id="hm_livescore_2305416_7268">0</span>
-// 1                </div>
-//0            </div>
-//
-//1            <div style="display:flex;flex-direction: row;justify-content: space-between">
-//0                <div style="text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">
-//
-//0                    <span style="display:inline-block;width:25px;text-align: center;">
-// 0                        <img src="http://static.hltv.org//images/flag/bg.gif" alt="" height="12" width="18" class="flagFix"/>
-//0                    </span>
-//
-//1                    <span style="">Bpro</span>
-//0                </div>
-//1                <div style="flex-shrink: 0;padding-left:2px;padding-right:2px;">
-//0                    <span id="hm_livescore_2305416_6762">0 </span>
-//1                </div>
-//1            </div>
-//
-//0         </div>
-//
-//0     </div>
-//
-// </a>
